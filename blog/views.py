d675ad7fad25c5cf9b8c9b210ecb76b98c8c21bc
Kolamin <gofranich@gmail.com>
2019-10-05 14:54:07 +0600

    release V5

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def posts_list(request):
-    return render(request, 'blog\index.html')
+    n = ['Oleg', 'Masha', 'Olga', 'Ksu']
+    return render(request, 'blog\index.html', context={'names': n})
